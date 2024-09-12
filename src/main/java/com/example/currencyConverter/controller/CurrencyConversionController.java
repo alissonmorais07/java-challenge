74c604f18059024e1406e7699941c4888a086741
alisson <alissonmorais177@gmail.com>
2024-09-12 18:00:10 -0300

    Project documentation using Swagger

--- src/main/java/com/example/currencyConverter/controller/CurrencyConversionController.java
+++ src/main/java/com/example/currencyConverter/controller/CurrencyConversionController.java
@@ -3,14 +3,15 @@ package com.example.currencyConverter.controller;
 import com.example.currencyConverter.dto.request.ExchangeRequest;
 import com.example.currencyConverter.dto.response.ExchangeResponse;
 import com.example.currencyConverter.model.CurrencyConversion;
-import com.example.currencyConverter.repository.CurrencyRepository;
 import com.example.currencyConverter.service.CurrencyConversionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -18,7 +19,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class CurrencyConversionController {
 
-        private final CurrencyConversionService currencyConversionService;
+    private final CurrencyConversionService currencyConversionService;
 
     @Autowired
     private CurrencyConversionService conversionService;
@@ -27,13 +28,23 @@ public class CurrencyConversionController {
         this.currencyConversionService = currencyConversionService;
     }
 
+    @Operation(description = "Perform currency conversion and save the transaction")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns the currency conversion record."),
+            @ApiResponse(responseCode = "400", description = "Returns request processing error.")
+    })
     @PostMapping("/convert")
     public ExchangeResponse convert(@Valid @RequestBody ExchangeRequest request) {
         return conversionService.convertCurrency(request);
     }
 
+    @Operation(description = "Displays the general and user conversion list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns the general and user conversion list."),
+            @ApiResponse(responseCode = "400", description = "Returns request processing error.")
+    })
     @GetMapping("/list")
-    public ResponseEntity<List<CurrencyConversion>> findCurrencyRepositoryByUser(@RequestParam(required = false) Long userID){
+    public ResponseEntity<List<CurrencyConversion>> findCurrencyRepositoryByUser(@RequestParam(required = false) Long userID) {
         List<CurrencyConversion> conversions;
 
         if (userID == null) {
